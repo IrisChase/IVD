51e3f60434e04f468b25589381eaa6ef950a72f2
Iris Chase <iris@enesda.com>
2020-07-05 23:39:22 -0400

    Add script to execute reprodyne-based tests concurrently. Closes #22

---
+++ src/tests/runtime/concurrenttest.py
@@ -0,0 +1,36 @@
+import concurrent.futures
+import subprocess
+import os
+import sys
+
+nope, cutable = sys.argv
+
+def do_all(IMES):
+    def exec(arglist):
+        try:
+            subprocess.run(arglist, check=True)
+        except:
+            print("Failed for: %s" % (arglist[2]))
+            print("\n")
+
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for i in IMES:
+            futures.append(executor.submit(exec, i))
+        
+        for f in concurrent.futures.as_completed(futures):
+            pass
+
+IME = []
+
+for fileName in os.listdir():
+    base, ext = os.path.splitext(fileName)
+    if ext == '.ivd':
+        x3thpath = os.path.join("reprodyne", base + ".x3th")
+        IME.append([cutable, 'play', fileName, x3thpath])
+        
+
+
+do_all(IME)
+
