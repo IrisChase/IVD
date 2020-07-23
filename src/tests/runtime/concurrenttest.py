866956da76b1a7156b1660e1d869c9783356e7b6
Iris Chase <iris@enesda.com>
2020-07-22 22:45:40 -0400

    Make reprodyne execution script skip unvalidated tests

--- src/tests/runtime/concurrenttest.py
+++ src/tests/runtime/concurrenttest.py
@@ -28,6 +28,12 @@ for fileName in os.listdir():
     base, ext = os.path.splitext(fileName)
     if ext == '.ivd':
         x3thpath = os.path.join("reprodyne", base + ".x3th")
+        
+        #is it even existant
+        if not os.path.isfile(x3thpath):
+            print("Could not find x3th for: %s, skipping..." % (fileName))
+            continue
+
         IME.append([cutable, 'play', fileName, x3thpath])
         
 
