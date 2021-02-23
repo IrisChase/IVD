b0bd3e186c2abbe874fd523214b3a6219defa68c
Iris Chase <iris@enesda.com>
2021-02-23 12:01:59 -0500

    FORGOT TO TRACK THESE TOO REEE

---
+++ src/widgets/boxlayout.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "user_include/cpp/IVD_cpp.h"
+
+namespace IVD
+{
+
+namespace std_widgets
+{
+
+class BoxLayout : public UserLayout
+{
+    const Angle Adjacent;
+
+public:
+    BoxLayout(const Angle theAngel): Adjacent(theAngel) {}
+    ~BoxLayout() override {}
+
+    virtual FillPrecedence get_fill_precedence(const Angle angle) final
+    { return any_greedy_children_for_angle(angle); }
+
+    virtual void shape(const GeometryProposal officialProposal) final;
+
+    virtual void draw(Canvas theCanvas) final
+    { render_children_unordered(); }
+
+    virtual void bubble_children() final
+    { bubble_children_unordered(); }
+};
+
+class VboxLayout : public BoxLayout
+{
+public:
+    VboxLayout(): BoxLayout(Angle::Vertical) {}
+    ~VboxLayout() override {}
+};
+
+class HboxLayout : public BoxLayout
+{
+public:
+    HboxLayout(): BoxLayout(Angle::Horizontal) {}
+    ~HboxLayout() override {}
+};
+
+
+}//std_widgets
+}//IVD
---
+++ src/widgets/stacklayout.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "user_include/cpp/IVD_cpp.h"
+
+namespace IVD
+{
+namespace std_widgets
+{
+
+class StackLayout : public UserLayout
+{
+    //basically reverse insertion order
+    void applyToChildrenInBubbleOrder(std::function<void(Element)> fun)
+    {
+        std::vector<Element> elements;
+
+        applyToChildren([&](Element elem)
+        { elements.push_back(elem); });
+
+        for(auto rit = elements.rbegin(); rit != elements.rend(); ++rit)
+            fun(*rit);
+    }
+
+public:
+    virtual FillPrecedence get_fill_precedence(const Angle angel)
+    { return any_greedy_children_for_angle(angel); }
+
+    virtual void shape(const GeometryProposal officialProposal);
+
+    virtual void draw(Canvas theCanvas)
+    {
+        applyToChildren([&](Element elem)
+        { elem.render(); });
+    }
+
+    virtual void bubble_children()
+    {
+        applyToChildrenInBubbleOrder([&](Element elem)
+        { elem.bubble(); });
+    }
+};
+
+}//std_widgets
+}//IVD
